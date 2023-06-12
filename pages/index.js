e8464981978136dc7bab2ca63b6272854874d2b7
Jakov Knezevic <knezevic.jakov@gmail.com>
2023-06-12 17:34:30 +0200

    Portfolio update.

--- pages/index.js
+++ pages/index.js
@@ -9,12 +9,13 @@ import avatar from "../public/avatar.jpeg";
 import design from "../public/design.png";
 import code from "../public/code.png";
 import consulting from "../public/consulting.png";
-// import web1 from "../public/web1.png";
-// import web2 from "../public/web2.png";
-// import web3 from "../public/web3.png";
-// import web4 from "../public/web4.png";
-// import web5 from "../public/web5.png";
-// import web6 from "../public/web6.png";
+
+import plahutica from "../public/plahutica.png";
+import crwn_clothing from "../public/crwn-clothing.svg";
+import recipe_o_mat from "../public/recipe-o-mat.png";
+
+// Barlow font for headings
+// Roboto Slab, Roboto or Open Sans font for body
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(false);
@@ -29,8 +30,8 @@ export default function Home() {
 
       <main className="bg-gray-50 px-10 dark:bg-gray-800 md:px-20 lg:px-20">
         <section className="min-h-screen">
-          <nav className="py-10 mb-20 flex justify-between 2xl:py-5">
-            <h1 className="text-base font-burtons dark:text-gray-100 md:text-xl 3xl:text-2xl">
+          <nav className="mb-20 flex justify-between py-10 2xl:py-5">
+            <h1 className="font-burtons text-base dark:text-gray-100 md:text-xl 3xl:text-2xl">
               Jakov Knezevic
             </h1>
             <ul className="flex items-center">
@@ -42,7 +43,7 @@ export default function Home() {
               </li>
               <li>
                 <a
-                  className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8 lg:hover:transition-all lg:hover:bg-gradient-to-r lg:hover:from-cyan-600 lg:hover:to-teal-600 3xl:text-lg"
+                  className="ml-8 rounded-md bg-gradient-to-r from-cyan-500 to-teal-500 px-4 py-2 text-white lg:hover:bg-gradient-to-r lg:hover:from-cyan-600 lg:hover:to-teal-600 lg:hover:transition-all 3xl:text-lg"
                   href="https://europa.eu/europass/eportfolio/screen/share/ce587334-2833-47ab-b396-048b673417ae?lang=en"
                   target="_blank"
                   rel="noreferrer"
@@ -54,19 +55,19 @@ export default function Home() {
           </nav>
 
           <div className="text-center md:p-10">
-            <h2 className="text-4xl py-2 text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-teal-500 font-medium md:text-5xl 2xl:text-6xl 3xl:text-6xl">
+            <h2 className="bg-gradient-to-r from-cyan-500 to-teal-500 bg-clip-text py-2 text-4xl font-medium text-transparent md:text-5xl 2xl:text-6xl 3xl:text-6xl">
               Jakov Knezevic
             </h2>
-            <h3 className="text-2xl py-2 dark:text-gray-100 md:text-2xl 2xl:text-3xl">
+            <h3 className="py-2 text-2xl dark:text-gray-100 md:text-2xl 2xl:text-3xl">
               Frontend developer
             </h3>
-            <p className="text-base py-5 leading-8 max-w-lg mx-auto text-gray-800 dark:text-gray-300 md:text-lg 2xl:text-xl">
+            <p className="mx-auto max-w-lg py-5 text-base leading-8 text-gray-800 dark:text-gray-300 md:text-lg 2xl:text-xl">
               Creating new software and websites while also refurbishing and
               upgrading the old ones. Feel free to get in contact for more
               information and a possible collaboration!
             </p>
           </div>
-          <div className="text-5xl flex justify-center my-20 text-gray-600 dark:text-gray-300 2xl:text-6xl 2xl:my-10 3xl:my-14">
+          <div className="my-20 flex justify-center text-5xl text-gray-600 dark:text-gray-300 2xl:my-10 2xl:text-6xl 3xl:my-14">
             <a
               href="https://github.com/jknezevic1712/"
               target="_blank"
@@ -91,80 +92,80 @@ export default function Home() {
               <AiFillMail />
             </a>
           </div>
-          <div className="relative mx-auto rounded-full w-52 h-52 mt-32 overflow-hidden filter shadow-2xl md:mb-0 md:mt-20 md:h-96 md:w-96 lg:mt-32">
+          <div className="relative mx-auto mt-32 h-52 w-52 overflow-hidden rounded-full shadow-2xl filter md:mb-0 md:mt-20 md:h-96 md:w-96 lg:mt-32">
             <Image src={avatar} alt="" layout="fill" objectFit="cover" />
           </div>
         </section>
 
         <section>
           <div className="mb-20">
-            <h3 className="text-3xl mb-5 text-center dark:text-gray-100 md:text-left 3xl:text-4xl">
+            <h3 className="mb-5 text-center text-3xl dark:text-gray-100 md:text-left 3xl:text-4xl">
               Services I offer
             </h3>
-            <p className="text-base py-2 leading-8 text-center text-gray-800 dark:text-gray-300 md:text-left md:text-lg 3xl:text-xl">
+            <p className="py-2 text-center text-base leading-8 text-gray-800 dark:text-gray-300 md:text-left md:text-lg 3xl:text-xl">
               Since the beginning of my journey as a frontend web and software
               developer, I&apos;ve done remote and on site work for{" "}
-              <span className="text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-teal-500 lg:hover:transition-all lg:hover:bg-gradient-to-r lg:hover:from-cyan-600 lg:hover:to-teal-600">
+              <span className="bg-gradient-to-r from-cyan-500 to-teal-500 bg-clip-text text-transparent lg:hover:bg-gradient-to-r lg:hover:from-cyan-600 lg:hover:to-teal-600 lg:hover:transition-all">
                 <a href="https://www.libelle.com/">Libelle IT Group</a>
               </span>{" "}
               and{" "}
-              <span className="text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-teal-500 lg:hover:transition-all lg:hover:bg-gradient-to-r lg:hover:from-cyan-600 lg:hover:to-teal-600">
+              <span className="bg-gradient-to-r from-cyan-500 to-teal-500 bg-clip-text text-transparent lg:hover:bg-gradient-to-r lg:hover:from-cyan-600 lg:hover:to-teal-600 lg:hover:transition-all">
                 <a href="https://www.ovis-software.com/">Ovis software</a>
               </span>{" "}
               while also working on personal development and side projects that
               continuously keep advancing me to the next levels.
             </p>
-            <p className="text-base py-2 leading-8 text-center text-gray-800 dark:text-gray-300 md:text-left md:text-lg 3xl:text-xl">
+            <p className="py-2 text-center text-base leading-8 text-gray-800 dark:text-gray-300 md:text-left md:text-lg 3xl:text-xl">
               I offer from a wide range of services, including web and software
               development.
             </p>
           </div>
 
-          <div className="lg:flex gap-10 justify-center">
-            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-gray-50">
+          <div className="gap-10 lg:flex lg:flex-wrap lg:justify-center lg:gap-5 xl:flex-nowrap xl:gap-10">
+            <div className="my-10 flex flex-1 basis-1/3 flex-col items-center justify-start rounded-xl p-10 text-center shadow-lg dark:bg-gray-50">
               <Image src={code} alt="" width={100} height={100} />
-              <h3 className="text-lg font-medium pt-8 pb-2 2xl:text-2xl">
+              <h3 className="pb-2 pt-8 text-lg font-medium 2xl:text-2xl">
                 Modern websites and software
               </h3>
-              <p className="py-2 2xl:text-lg">
+              <p className="max-w-sm py-2 2xl:text-lg">
                 Creating web apps and software using best coding practices and
                 elegant designs suited for your needs so that your website or
                 software looks modern and up-to-date.
               </p>
-              <h4 className="py-4 text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-teal-500 2xl:text-xl">
+              <h4 className="bg-gradient-to-r from-cyan-500 to-teal-500 bg-clip-text py-4 text-transparent 2xl:text-xl">
                 Most used tools
               </h4>
-              <div className="flex justify-center items-center 2xl:text-lg">
+              <div className="flex items-center justify-center 2xl:text-lg">
                 <div className="mr-5 md:mr-10">
-                  <p className="text-gray-800 py-1">ReactJS</p>
-                  <p className="text-gray-800 py-1">Typescript</p>
-                  <p className="text-gray-800 py-1">jQuery</p>
+                  <p className="py-1 text-gray-800">React</p>
+                  <p className="py-1 text-gray-800">Angular</p>
+                  <p className="py-1 text-gray-800">Typescript</p>
                 </div>
                 <div>
-                  <p className="text-gray-800 py-1">Tailwind CSS</p>
-                  <p className="text-gray-800 py-1">Styled Components</p>
-                  <p className="text-gray-800 py-1">Material UI</p>
+                  <p className="py-1 text-gray-800">Tailwind CSS</p>
+                  <p className="py-1 text-gray-800">Styled Components</p>
+                  <p className="py-1 text-gray-800">Material UI</p>
                 </div>
               </div>
             </div>
 
-            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-gray-50">
+            <div className="my-10 flex flex-1 basis-1/3 flex-col items-center justify-start rounded-xl p-10 text-center shadow-lg dark:bg-gray-50">
               <Image src={design} alt="" width={100} height={100} />
-              <h3 className="text-lg font-medium pt-8 pb-2 2xl:text-2xl">
+              <h3 className="pb-2 pt-8 text-lg font-medium 2xl:text-2xl">
                 Refurbishing old designs and software
               </h3>
-              <p className="py-2 2xl:text-lg">
+              <p className="max-w-sm py-2 2xl:text-lg">
                 Updating your websites and software both visually and under the
                 hood so that it works better with higher performance while
                 looking modern and beautiful once more.
               </p>
             </div>
-            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-gray-50">
+            <div className="my-10 flex flex-1 basis-1/3 flex-col items-center justify-start rounded-xl p-10 text-center shadow-lg dark:bg-gray-50">
               <Image src={consulting} alt="" width={100} height={100} />
-              <h3 className="text-lg font-medium pt-8 pb-2 2xl:text-2xl">
+              <h3 className="pb-2 pt-8 text-lg font-medium 2xl:text-2xl">
                 Collaborations
               </h3>
-              <p className="py-2 2xl:text-lg">
+              <p className="max-w-sm py-2 2xl:text-lg">
                 I believe in collaborations on all sorts of projects in order
                 for mutual benefits and advancement. Writing this, feel free to
                 contact me via options listed on the top of the page.
@@ -175,102 +176,92 @@ export default function Home() {
 
         <section>
           <div className="py-20">
-            <h3 className="text-3xl mb-5 text-center dark:text-gray-100 md:text-left 3xl:text-4xl">
+            <h3 className="mb-5 text-center text-3xl dark:text-gray-100 md:text-left 3xl:text-4xl">
               Portfolio
             </h3>
-            <p className="text-base py-2 leading-8 text-center text-gray-800 dark:text-gray-300 md:text-left md:text-lg 3xl:text-xl">
+            <p className="py-2 text-center text-base leading-8 text-gray-800 dark:text-gray-300 md:text-left md:text-lg 3xl:text-xl">
               Born in 1998 in Pakrac, Croatia. Graduated at Tehnička škola
               Daruvar becoming a{" "}
-              <span className="text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-teal-500">
+              <span className="bg-gradient-to-r from-cyan-500 to-teal-500 bg-clip-text text-transparent">
                 computer technician
               </span>
               . After that, I enrolled at the Faculty of Organization and
               Informatics in Varaždin, where I obtained degree of{" "}
-              <span className="text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-teal-500">
+              <span className="bg-gradient-to-r from-cyan-500 to-teal-500 bg-clip-text text-transparent">
                 Bachelors in Informatics
               </span>{" "}
               and I am currently pursuing{" "}
-              <span className="text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-teal-500">
+              <span className="bg-gradient-to-r from-cyan-500 to-teal-500 bg-clip-text text-transparent">
                 Master of Informatics
               </span>{" "}
               degree.
             </p>
-            <p className="text-base py-2 leading-8 text-center text-gray-800 dark:text-gray-300 md:text-left md:text-lg 3xl:text-xl">
+            <p className="py-2 text-center text-base leading-8 text-gray-800 dark:text-gray-300 md:text-left md:text-lg 3xl:text-xl">
               Personality wise, I am interested in programming and music. I also
               believe in team work and communication as they are, in my opinion,
               crucial to achieving{" "}
-              <span className="text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-teal-500">
+              <span className="bg-gradient-to-r from-cyan-500 to-teal-500 bg-clip-text text-transparent">
                 success
               </span>{" "}
               and{" "}
-              <span className="text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-teal-500">
+              <span className="bg-gradient-to-r from-cyan-500 to-teal-500 bg-clip-text text-transparent">
                 progress
               </span>
               .
             </p>
           </div>
-          {/* <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
-            <div className="basis-1/3 flex-1">
-              <Image
-                src={web1}
-                alt=""
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-              />
+        </section>
+
+        <section>
+          <div className="py-20">
+            <h3 className="mb-5 text-center text-3xl dark:text-gray-100 md:text-left 3xl:text-4xl">
+              Personal projects
+            </h3>
+            <div className="gap-10 lg:flex lg:flex-wrap lg:justify-center lg:gap-5 xl:flex-nowrap xl:gap-10">
+              <div className="my-10 flex flex-1 basis-1/3 flex-col items-center justify-start rounded-xl p-10 text-center shadow-lg dark:bg-gray-50">
+                <Image src={plahutica} alt="" width={100} height={100} />
+                <a
+                  href="https://plahutica.vercel.app/"
+                  target="_blank"
+                  className="rounded-md bg-gradient-to-r from-cyan-500 to-teal-500 bg-clip-text px-4 py-2 pb-2 pt-8 text-lg font-medium text-transparent lg:hover:opacity-75 lg:hover:transition-all 2xl:text-2xl"
+                >
+                  Plahutica
+                </a>
+                <p className="max-w-sm py-2 2xl:text-lg">
+                  Travelling on a budget guide here to help you out with useful
+                  ideas or advices for your journey.
+                </p>
+              </div>
+
+              <div className="my-10 flex flex-1 basis-1/3 flex-col items-center justify-start rounded-xl p-10 text-center shadow-lg dark:bg-gray-50">
+                <Image src={recipe_o_mat} alt="" width={100} height={100} />
+                <a
+                  href="https://recipe-o-mat.web.app/"
+                  target="_blank"
+                  className="rounded-md bg-gradient-to-r from-cyan-500 to-teal-500 bg-clip-text px-4 py-2 pb-2 pt-8 text-lg font-medium text-transparent lg:hover:opacity-75 lg:hover:transition-all 2xl:text-2xl"
+                >
+                  Recipe-O-Mat
+                </a>
+                <p className="max-w-sm py-2 2xl:text-lg">
+                  Check out some amazing recipes or share yours with others!
+                </p>
+              </div>
+
+              <div className="my-10 flex flex-1 basis-1/3 flex-col items-center justify-start rounded-xl p-10 text-center shadow-lg dark:bg-gray-50">
+                <Image src={crwn_clothing} alt="" width={100} height={100} />
+                <a
+                  href="https://crwn-clothing-jk.vercel.app/"
+                  target="_blank"
+                  className="rounded-md bg-gradient-to-r from-cyan-500 to-teal-500 bg-clip-text px-4 py-2 pb-2 pt-8 text-lg font-medium text-transparent lg:hover:opacity-75 lg:hover:transition-all 2xl:text-2xl"
+                >
+                  Crown Clothing
+                </a>
+                <p className="max-w-sm py-2 2xl:text-lg">
+                  Web shop with various products.
+                </p>
+              </div>
             </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                src={web2}
-                alt=""
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-              />
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                src={web3}
-                alt=""
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-              />
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                src={web4}
-                alt=""
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-              />
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                src={web5}
-                alt=""
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-              />
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                src={web6}
-                alt=""
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-              />
-            </div>
-          </div> */}
+          </div>
         </section>
       </main>
     </div>
