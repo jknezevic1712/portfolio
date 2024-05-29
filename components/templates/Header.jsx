d456de80c1628dccbadff3459626f81e9cff6dd3
Jakov Knezevic <knezevic.jakov@gmail.com>
2024-05-29 11:36:48 +0200

    CV update.

--- components/templates/Header.jsx
+++ components/templates/Header.jsx
@@ -3,31 +3,31 @@ import { BsFillMoonStarsFill } from 'react-icons/bs';
 import Anchor from '../atoms/Anchor';
 
 export default function Header({ setDarkMode }) {
-  return (
-    <section className="mb-20 w-full md:mb-0">
-      <nav className="flex justify-between py-10 2xl:py-5">
-        <h1 className="font-burtons text-base dark:text-gray-100 md:text-xl 3xl:text-2xl">
-          Jakov Knezevic
-        </h1>
-        <ul className="flex items-center">
-          <li>
-            <BsFillMoonStarsFill
-              className="cursor-pointer text-2xl dark:text-gray-100 3xl:text-3xl"
-              onClick={() => setDarkMode((v) => !v)}
-            />
-          </li>
-          <li>
-            <Anchor
-              href={
-                'https://europa.eu/europass/eportfolio/screen/share/27dfb314-7d16-450a-b877-c52efea882b9?lang=en'
-              }
-              type="CV"
-            >
-              Resume
-            </Anchor>
-          </li>
-        </ul>
-      </nav>
-    </section>
-  );
+	return (
+		<section className="mb-20 w-full md:mb-0">
+			<nav className="flex justify-between py-10 2xl:py-5">
+				<h1 className="font-burtons text-base dark:text-gray-100 md:text-xl 3xl:text-2xl">
+					Jakov Knezevic
+				</h1>
+				<ul className="flex items-center">
+					<li>
+						<BsFillMoonStarsFill
+							className="cursor-pointer text-2xl dark:text-gray-100 3xl:text-3xl"
+							onClick={() => setDarkMode((v) => !v)}
+						/>
+					</li>
+					<li>
+						<Anchor
+							href={
+								'https://europa.eu/europass/eportfolio/screen/share/0ddbfb19-7efe-450f-828d-45577e217a12?lang=en'
+							}
+							type="CV"
+						>
+							Resume
+						</Anchor>
+					</li>
+				</ul>
+			</nav>
+		</section>
+	);
 }
