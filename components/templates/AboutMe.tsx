a3c0b5954862254c8c560df341264ea04482b858
jknezevic-cirtuo <jakov.knezevic@cirtuo.com>
2026-05-11 21:45:51 +0200

    About update

--- components/templates/AboutMe.tsx
+++ components/templates/AboutMe.tsx
@@ -1,58 +1,17 @@
 import Paragraph from '../atoms/Paragraph';
 
 export default function AboutMe() {
-  return (
-    <section className="my-5 w-full max-w-7xl">
-      <h3 className="mb-5 text-center text-3xl dark:text-gray-100 md:text-left 3xl:text-4xl">
-        About me
-      </h3>
-      <Paragraph type="CONTENT">
-        Born in 1998 in Pakrac, Croatia. Graduated at Technical school in
-        Daruvar becoming a{' '}
-        <span className="bg-linear-to-r from-cyan-500 to-teal-500 bg-clip-text text-transparent">
-          computer technician
-        </span>
-        . After that, I enrolled at the Faculty of Organization and Informatics
-        in Varaždin, where I obtained degree of{' '}
-        <span className="bg-linear-to-r from-cyan-500 to-teal-500 bg-clip-text text-transparent">
-          Bachelors in Informatics
-        </span>
-        .
-      </Paragraph>
+	return (
+		<section className="my-5 w-full max-w-7xl">
+			<h3 className="mb-5 text-center text-3xl dark:text-gray-100 md:text-left 3xl:text-4xl">
+				About
+			</h3>
 
-      <Paragraph type="CONTENT">
-        Personality wise, I am interested in programming, music and travelling.
-        I also believe in team work and communication as they are, in my
-        opinion, crucial to achieving{' '}
-        <span className="bg-linear-to-r from-cyan-500 to-teal-500 bg-clip-text text-transparent">
-          success
-        </span>{' '}
-        and{' '}
-        <span className="bg-linear-to-r from-cyan-500 to-teal-500 bg-clip-text text-transparent">
-          progress
-        </span>
-        . In my free time, I try to explore new patterns/ways of building all
-        kinds of different features to stay up to date with the latest additions
-        to the popular libraries and packages I use while learning new things
-        and gaining more experience as well.
-      </Paragraph>
-
-      <Paragraph type="CONTENT">
-        Additionally, I strive to become part of a team that pushes each other
-        every day to{' '}
-        <span className="bg-linear-to-r from-cyan-500 to-teal-500 bg-clip-text text-transparent">
-          become better
-        </span>{' '}
-        by{' '}
-        <span className="bg-linear-to-r from-cyan-500 to-teal-500 bg-clip-text text-transparent">
-          learning from our mistakes
-        </span>{' '}
-        and{' '}
-        <span className="bg-linear-to-r from-cyan-500 to-teal-500 bg-clip-text text-transparent">
-          working together to solve them
-        </span>{' '}
-        so that everyone can hopefully learn something along the way!
-      </Paragraph>
-    </section>
-  );
+			<Paragraph type="CONTENT">
+				Software Engineer with 5+ years shipping web applications for enterprise
+				and startups. Building scalable frontends, modernizing legacy codebases,
+				and owning features from concept to deployment.
+			</Paragraph>
+		</section>
+	);
 }
