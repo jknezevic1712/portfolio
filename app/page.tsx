6694871721b3adb2f8d001ee332a49e2b3649de1
jknezevic-cirtuo <jakov.knezevic@cirtuo.com>
2026-06-01 12:57:28 +0200

    Small refactor

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Hero from '@/components/templates/Hero';
-import About from '@/components/templates/About';
-import WorkExperience from '@/components/templates/WorkExperience';
-import Education from '@/components/templates/Education';
-import Skills from '@/components/templates/Skills';
-import SideProjects from '@/components/templates/SideProjects';
+import Hero from '@/components/organisms/Hero';
+import About from '@/components/organisms/About';
+import WorkExperience from '@/components/organisms/WorkExperience';
+import Education from '@/components/organisms/Education';
+import Skills from '@/components/organisms/Skills';
+import SideProjects from '@/components/organisms/SideProjects';
 
 export default function Home() {
 	return (
